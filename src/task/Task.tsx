f25f39613254494a782186a6116486d089e45ea4
Hasara-Thilakarathna <hasarathilakarathnaa4@gmail.com>
2023-12-15 18:46:14 +0530

    Create update task function

--- src/task/Task.tsx
+++ src/task/Task.tsx
@@ -1,15 +1,30 @@
 import './Task.css';
 import {TaskDto} from "../dto/TaskDto.ts";
-import {useId} from "react";
+import {ChangeEvent, useId, useState} from "react";
 import {useTaskDispatcher} from "../context/TaskContext.tsx";
+import {updateTask} from "../service/task-service.tsx";
 export function Task(task:TaskDto) {
     const id = useId();
     const taskDispatcher = useTaskDispatcher();
+    const [checked, setChecked] = useState(task.status);
+
+    function handleCheck(e:ChangeEvent<HTMLInputElement>){
+        updateTask(task).then(value => {
+            setChecked(!checked)
+        }).catch(err =>{
+            alert("Failed to update the task")
+        })
+    }
 
     return (
         <div id="task" className="d-flex justify-content-between align-items-center px-2 fs-5">
             <div className="d-flex gap-2 py-3">
-                <input id={id} type="checkbox"/>
+                <input
+                    onChange={handleCheck}
+                    checked={checked ?? false}
+                    id={id}
+                    className="form-check-input"
+                    type="checkbox"/>
                 <label htmlFor={id}>{task.description}</label>
             </div>
             <i id="btn-delete" className="bi bi-trash-fill fs-4"></i>
